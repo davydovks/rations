b59e343b018c5b3a091aa835ddf44c32faf21d7d
Konstantin Davydov <davydovks@mail.ru>
2025-01-30 23:37:53 +0500

    create tariff factory

---
+++ database/factories/TariffFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tariff>
+ */
+class TariffFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ration_name' => fake()->words(3, true),
+            'cooking_day_before' => rand(0,1) == 1
+        ];
+    }
+}
