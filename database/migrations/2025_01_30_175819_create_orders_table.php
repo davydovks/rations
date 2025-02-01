02bf63a9c0e37c02594b7d7d8af540961ec39597
Konstantin Davydov <davydovks@mail.ru>
2025-02-01 13:31:42 +0500

    create order table migration

---
+++ database/migrations/2025_01_30_175819_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Tariff;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->string('client_phone', 11)->unique();
+            $table->foreignIdFor(Tariff::class);
+            $table->string('schedule_type', 21);
+            $table->text('comment');
+            $table->date('first_date');
+            $table->date('last_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
