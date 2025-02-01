d523c1de048a3f11eb6c1cd44dee3ecc90cdc0b6
Konstantin Davydov <davydovks@mail.ru>
2025-02-01 13:32:21 +0500

    create order model

---
+++ app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ScheduleType;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'scheduleType' => ScheduleType::class,
+        ];
+    }
+
+    /**
+     * Get the tariff for this order
+     * @return BelongsTo<Tariff, Order>
+     */
+    public function tariff(): BelongsTo
+    {
+        return $this->belongsTo(Tariff::class);
+    }
+}
--- app/Models/Tariff.php
+++ app/Models/Tariff.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tariff extends Model
 {
     /** @use HasFactory<\Database\Factories\TariffFactory> */
     use HasFactory;
+
+    /**
+     * Get all orders with this tariff
+     * @return HasMany<Order, Tariff>
+     */
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
