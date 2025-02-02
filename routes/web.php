5bd66b390d69c6ab58592063437ad1984d432ac2
Konstantin Davydov <davydovks@mail.ru>
2025-02-02 21:46:13 +0500

    set redirect to orders from home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('orders.index');
 });
 
 Route::resource('orders', OrderController::class);
