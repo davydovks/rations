bddd798b9eea2625482af6ba94b9251f5ee77976
Konstantin Davydov <davydovks@mail.ru>
2025-02-02 13:03:54 +0500

    add order.create

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Http\Request;
+use App\Models\Tariff;
 
 class OrderController extends Controller
 {
@@ -16,5 +16,16 @@ class OrderController extends Controller
     public function show(Order $order)
     {
         return view('order.show', compact('order'));
+    }
+
+    public function create()
+    {
+        $tariffs = Tariff::pluck('ration_name', 'id');
+        $scheduleTypes = [
+            'EVERY_DAY' => 'Ежедневная доставка',
+            'EVERY_OTHER_DAY' => 'Доставка через день на 1 день',
+            'EVERY_OTHER_DAY_TWICE' => 'Доставка через день на 2 дня',
+        ];
+        return view('order.create', compact('tariffs', 'scheduleTypes'));
     }
 }
---
+++ resources/views/order/create.blade.php
@@ -0,0 +1,31 @@
+<x-layout>
+    <div class="grid">
+        <h1><a href="/orders/" class="link">Заказы</a> > Создать</h1>
+
+
+        {{ html()->form('POST')->route('orders.store')->open() }}
+            {{ html()->label('ФИО клиента','client_name')->class('attribute') }}
+            {{ html()->text('client_name') }}
+            <br>            
+            {{ html()->label('Телефон','client_phone')->class('attribute') }}
+            {{ html()->text('client_phone') }}
+            <br>
+            {{ html()->label('Тариф','tariff_id')->class('attribute') }}
+            {{ html()->select('tariff_id')
+                ->children($tariffs, fn($tariff, $id) => html()->option($tariff, $id))
+            }}
+            <br>
+            {{ html()->label('Расписание','schedule_type')->class('attribute') }}
+            {{ html()->select('schedule_type')
+                ->children($scheduleTypes, fn($description, $id) => html()->option($description, $id))
+            }}
+            <br>
+            {{ html()->label('Комментарий','comment')->class('attribute align-top') }}
+            {{ html()->textarea('comment') }}
+            <br>
+
+            {{ html()->submit("Сохранить")->class('btn mt-5') }}
+        {{ html()->form()->close() }}
+        
+    </div>
+</x-layout>
